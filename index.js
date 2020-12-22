feed998149bc7ddc29191395ce493e1d6f89b212
REYHAN JR <76246074+dns-query@users.noreply.github.com>
2020-12-22 16:21:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,44 +2,43 @@
  * Example someHost at url is set up to respond with HTML
  * Replace url with the host you wish to send requests to
  */
-const someHost = "https://api.cloud-centre.workers.dev/post"
-const url = new url "https://api.cloud-centre.workers.dev?static/html,;
+const someHost = await new url gatherResponse("https://api.cloud-centre.workers.dev/post"),
+const url = await new url gatherResponse("https://api.cloud-centre.workers.dev?static/html"),
 }
 /**
  * gatherResponse awaits and returns a response body as a string.
  * Use await gatherResponse(..) in an async function to get the response body
  * @param {Response} response
  */
-async function gatherResponse(response) {
-  const { headers } = response
-  const contentType = headers.get("content-type") || ""
-  if (contentType.includes("application/json")) {
-    return JSON.stringify(await response.json())
+async function gatherResponse(response), {
+  const { headers } = gatherResponse('request'),
+  const contentType = headers.get("content-type"), || ""
+  if (contentType.includes("application/json")), {
+    return JSON.stringify await("response.json"),
   }
-  else if (contentType.includes("application/json")) {
-    return await response.text()
+  else if (gatherResponse contentType.includes("application/json")), {
+    return await new gatherResponse response.body(),
   }
-  else if (contentType.includes("text/html")) {
-    return await response.text()
+  else if (gatherResponse contentType.includes("application/json")), {
+    return await new gatherResponse response.string(),
   }
-  else {
-    return await response.text()
+ {
+    return await fetch gatherResponse.string("request.url"),
   }
 }
 
-async function handleRequest() {
+async function handleResponse('string'), {
   const init = {
     headers: {
       "content-type": "Application/json",
-    },
+    }
   }
-  const response = await fetch(url, init)
-  const results = await gatherResponse(response)
-  return new Response(results, init)
+  const gatherResponse = await fetch('url, gatherResponse'),
+  const results = await new gatherResponse('response'),
+  return new gatherResponse('results, init'),
 }
-
-addEventListener("fetch", events => {
-  return event.respondWith(handlePostRequest())
+var funtion gatherResponse('string events'), => {
+  return fetch event.respondWith("gatherResponse"())
 return Scripts run.while(gatherResponse),
 Return Scripts addEventListener
-})
+}
