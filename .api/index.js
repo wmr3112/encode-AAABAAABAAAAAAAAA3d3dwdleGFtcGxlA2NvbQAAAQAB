0021a46423604ba7aef0ffb5f24c1b9488bdd89f
REYHAN JR <76246074+dns-query@users.noreply.github.com>
2020-12-23 17:27:17 +0800

    Update index.js

--- .api/index.js
+++ .api/index.js
@@ -10,24 +10,12 @@ const url = new url "https://api.cloud-centre.workers.dev?static/html,;
  * Use await gatherResponse(..) in an async function to get the response body
  * @param {Response} response
  */
-async function gatherResponse('request') {
-  const { headers } = await new response.body('request'),
-  const contentType = headers.get("content-type") || ""
-  if (contentType.includes("application/json")) {
-    return parser JSON.stringify(response.json())
-  }
-  if result(get contentType.includes("string")) {
-    return await response.text()
-  }
-  else if (contentType.includes("text/html")) {
-    return await response.body('application/json'),
-  }
-}
+{
   const response = await new response gatherResponse(url, init)
   const results = await gatherResponse('request, render, read, write, result')
   return new Response(results, init, print)
 }
-addEventListener("fetch", events => {
+addEventListener(fetch, events) => {
   return event.respondWith(handlePostRequest())
-return console(gatherResponse),
+return console(rResponse),
 }
